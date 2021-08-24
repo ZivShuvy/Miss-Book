0bb7052163133cd4319fdc505818c8e21814bf60
ZivShuvy <87780356+ZivShuvy@users.noreply.github.com>
2021-08-24 20:38:53 +0300

    Delete miss-book-proj directory

--- miss-book-proj/cmps/AppFooter.jsx
+++
@@ -1,7 +0,0 @@
-export function AppFooter() {
-    return (
-        <section className="app-footer">
-            © coffrerights 2021
-        </section>
-    )
-}
--- miss-book-proj/cmps/BookList.jsx
+++
@@ -1,8 +0,0 @@
-import { BookPreview } from './BookPreview.jsx'
-export function BookList({ books, onSelectBook }) {
-    return (
-        <div className='book-list'>
-            {books.map(book => <BookPreview key={book.id} book={book} onSelectBook={onSelectBook} />)}
-        </div>
-    )
-}
